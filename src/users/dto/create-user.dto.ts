59180e2950dc544e901a2caec7e113993ff5f292
정민석 <ms.jung@fanmaum.com>
2023-05-05 19:52:52 +0900

    CreateUserDto를 생성합니다.

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,10 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsEmail, IsString } from 'class-validator';
 
 export class CreateUserDto {
-  @IsNumber()
-  id: number;
-
   @IsString()
   name: string;
 
+  @IsEmail()
   @IsString()
   email: string;
 
