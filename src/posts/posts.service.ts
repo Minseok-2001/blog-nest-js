02235d436c71600ca894a67d44f427945fbe04f1
정민석 <ms.jung@fanmaum.com>
2023-05-08 00:20:55 +0900

    findPostsByUserId, createPost를 구현합니다.

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreatePostDto } from './dto/create-post.dto';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  public async findPostsByUserId(id: number) {
+    return this.prisma.post.findMany({
+      where: { authorId: id },
+    });
+  }
+
+  public async createPost(authorId: number, { title, content }: CreatePostDto) {
+    return this.prisma.post.create({
+      data: {
+        title,
+        content,
+        authorId,
+      },
+    });
+  }
+}
