2fc19cf7342ab2149f37bccadfa8ec8b031c2f80
정민석 <ms.jung@fanmaum.com>
2023-05-07 23:29:54 +0900

    UserModule에 JwtModule을 불러옵니다.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, JwtModule],
   controllers: [UsersController],
   exports: [UsersService],
   providers: [UsersService],
