19f7af7667d232a5421ee939f122b90d74fceb54
정민석 <ms.jung@fanmaum.com>
2023-05-08 00:19:57 +0900

    누락된 AuthModule을 AppModule에 추가합니다.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import jwtConfig from './config/jwt.config';
 import { ConfigModule } from '@nestjs/config';
 import { PostsModule } from './posts/posts.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { PostsModule } from './posts/posts.module';
       load: [jwtConfig],
       isGlobal: true,
     }),
+    AuthModule,
     PostsModule,
   ],
   controllers: [],
