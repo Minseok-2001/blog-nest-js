35de162e5a9e7cba1645165ecb1dd6564b0bfa5f
정민석 <ms.jung@fanmaum.com>
2023-05-07 23:28:29 +0900

    JwtModule을 불러옵니다.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,22 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import { JwtStrategy } from 'src/common/jwt/jwt-strategy';
 
 @Module({
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '60m' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  exports: [AuthModule, JwtModule],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
