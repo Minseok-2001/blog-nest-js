4fae5461de69e92b883dc3d295d6a0afc752154e
정민석 <ms.jung@fanmaum.com>
2023-05-07 17:08:29 +0900

    authGuard를 구현합니다.

---
+++ src/common/guard/auth.guard.ts
@@ -0,0 +1,33 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  BadRequestException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const authorizationHeader = request.headers.authorization;
+
+    if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
+      throw new UnauthorizedException('Invalid format');
+    }
+
+    const token = authorizationHeader.slice(7, authorizationHeader.length);
+
+    try {
+      const decoded = this.jwtService.verify(token);
+      request.user = decoded;
+
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+}
