5af013b3a4b0b52aae86350dabb72ff62659a7d3
정민석 <ms.jung@fanmaum.com>
2023-05-07 23:55:42 +0900

    UpdateUserDto에 유저가 알 수 없는 ID 필드를 제거합니다.

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,10 +1,6 @@
-import { IsNumber, IsOptional, IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserDto {
-  @IsOptional()
-  @IsNumber()
-  id?: number;
-
   @IsOptional()
   @IsString()
   name?: string;
