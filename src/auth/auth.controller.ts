0d975c84211a4d7348313b4166ceb6b6a2383213
정민석 <ms.jung@fanmaum.com>
2023-05-07 23:28:19 +0900

    AuthController를 구현합니다.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LoginUserDto } from './dto/login-user.dto';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() createUserDto: CreateUserDto) {
+    const user = await this.authService.register(createUserDto);
+    console.log(user);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    const token = this.authService.generateToken(user.id);
+    return { token };
+  }
+
+  @Post('login')
+  async login(@Body() loginUserDto: LoginUserDto) {
+    const user = await this.authService.login(loginUserDto);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    const token = this.authService.generateToken(user.id);
+    return { token };
+  }
 }
