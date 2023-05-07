3c2050cc992d1e0cf550a2be6e612bef820171b3
정민석 <ms.jung@fanmaum.com>
2023-05-08 00:20:27 +0900

    PostsController에 글 조회와 생성 엔드포인트를 추가합니다.

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { PostsService } from './posts.service';
+import { CurrentUser } from 'src/common/decorator/current-user.decorator';
+import { CurrentUserDto } from 'src/users/dto/current-user.dto';
+import { CreatePostDto } from './dto/create-post.dto';
+import { AuthGuard } from 'src/common/guard/auth.guard';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+
+  @UseGuards(AuthGuard)
+  @Get()
+  public findPostsByUserId(@CurrentUser() { id }: CurrentUserDto) {
+    return this.postsService.findPostsByUserId(id);
+  }
+
+  @UseGuards(AuthGuard)
+  @Post()
+  public createPost(
+    @CurrentUser() { id }: CurrentUserDto,
+    @Body() dto: CreatePostDto,
+  ) {
+    console.log(id);
+    return this.postsService.createPost(id, dto);
+  }
 }
