477cb32568ebb18655901bd86d6f8fd15b4c0a02
정민석 <ms.jung@fanmaum.com>
2023-06-24 14:39:26 +0900

    CreateCommentDto 설명 추가

--- backend/src/comments/dto/create-comment.dto.ts
+++ backend/src/comments/dto/create-comment.dto.ts
@@ -1,9 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsString } from 'class-validator';
 
 export class CreateCommentDto {
   @IsNumber()
   postId: number;
 
+  @ApiProperty({
+    example: '안녕',
+  })
   @IsString()
   content: string;
 }
