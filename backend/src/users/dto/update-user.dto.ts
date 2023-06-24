9d7306a842e7bf7022f2fc8dec9879b926261d13
정민석 <ms.jung@fanmaum.com>
2023-06-24 14:35:28 +0900

    UpdateUserDto swagger 설명 추가

--- backend/src/users/dto/update-user.dto.ts
+++ backend/src/users/dto/update-user.dto.ts
@@ -1,14 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserDto {
+  @ApiProperty({
+    example: '홍길동',
+  })
   @IsOptional()
   @IsString()
   name?: string;
 
+  @ApiProperty({
+    example: 'test@test.com',
+  })
   @IsOptional()
   @IsString()
   email?: string;
 
+  @ApiProperty({
+    example: 'somePassword',
+  })
   @IsOptional()
   @IsString()
   password?: string;
